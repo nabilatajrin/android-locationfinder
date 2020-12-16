3c8cf8a4bd38d48c2c464ebd4309da52012923bd
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-16 18:06:34 +0600

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/brishty/locationfinder/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/brishty/locationfinder/ExampleInstrumentedTest.java
@@ -3,10 +3,8 @@ package com.brishty.locationfinder;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
 
 /**
@@ -20,7 +18,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.brishty.locationfinder", appContext.getPackageName());
     }
 }
